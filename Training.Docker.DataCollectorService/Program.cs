eccefca471ba5e4bafa26f04934eb3ea6c474901
Łukasz Kępa <lukasz.kepa@cybercom.com>
2019-03-06 16:10:34 +0100

    Initial project structure

---
+++ Training.Docker.DataCollectorService/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Hosting;
+using Rebus.ServiceProvider;
+using Rebus.Transport.InMem;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Training.Docker.DataCollectorService
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var inputQueueName = Assembly.GetExecutingAssembly().GetName().Name;
+
+            var host = new HostBuilder()
+                .ConfigureServices(services =>
+                {
+                    services.AddRebus(conf =>
+                    {
+                        conf.Transport(t => t.UseInMemoryTransport(new InMemNetwork(true), inputQueueName));
+                        return conf;
+                    });
+                })
+                .UseConsoleLifetime()
+                .Build();
+
+            host.Services.UseRebus();
+
+            await host.RunAsync();
+        }
+    }
+}
